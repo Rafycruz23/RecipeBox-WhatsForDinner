49521304bf607f18fd029088980eed63c5a63295
Rafael <rafycruz23@live.com>
2023-03-02 11:23:38 -0500

    added navbar component, added Bootstrap

---
+++ whatsfordinner/src/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="#">Logo</a>
+        <a href="#">Plan My Meals</a>
+        <a href="#">All Recipes</a>
+        
+    </nav>
+}
