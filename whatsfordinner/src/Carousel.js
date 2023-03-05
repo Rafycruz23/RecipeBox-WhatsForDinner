f209dd4a8bbb8b259e804cb7ed553010ad657477
Rafael <rafycruz23@live.com>
2023-03-05 11:01:05 -0500

    added the carousel component from Bootstrap

---
+++ whatsfordinner/src/Carousel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+//Extracted the code below from Bootstrap Doc 
+export default function Carousel() {
+  return <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+  
+}
