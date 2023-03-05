64b524dffc4480f4fb39afb3036f9060312ba301
Rafael <rafycruz23@live.com>
2023-03-05 18:01:02 -0500

    axios get and API started, more work needed

--- whatsfordinner/src/App.js
+++ whatsfordinner/src/App.js
@@ -7,7 +7,6 @@ import "./styles.css"
 export default function App() {
   return <div>
     <Navbar />
-    <h1>Main Page APP.JS is working</h1>
     <CarouselSlide />
   </div>
 };
--- whatsfordinner/src/Navbar.js
+++ whatsfordinner/src/Navbar.js
@@ -1,7 +1,18 @@
 import {Link} from "react-router-dom"; //this replaces the a tags with Link, to move around pages. 
-
-
+import axios from 'axios';
 export default function Navbar(){
+
+
+    //AXIOS used to GET data from API. NEEDS WORK!!!!! Link works but function itself not. 
+
+const getRecipes = () =>{
+   
+axios.get('https://api.spoonacular.com/recipes/random?apiKey=5a312119fa95425c8af9a9236717e2b5').then(function
+    (response){
+console.log(response);
+    })
+}
+
     return <nav className="nav">
         <Link to="/">Logo</Link>
         <ul>
@@ -9,7 +20,7 @@ export default function Navbar(){
             <li><Link to="/all">All Recipes</Link></li>
             <form class="d-flex" role="search">
             <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-outline-success" onClick={getRecipes}>Search</button>
             </form>
         </ul> 
     </nav>
