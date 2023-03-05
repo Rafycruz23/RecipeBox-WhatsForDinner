37953b21174ab5082fca975c8a643ce9ad283b25
Rafael <rafycruz23@live.com>
2023-03-05 18:33:33 -0500

    added logo and styling

--- whatsfordinner/src/Navbar.js
+++ whatsfordinner/src/Navbar.js
@@ -1,20 +1,21 @@
+import logo from './assets/Logo.png'
 import {Link} from "react-router-dom"; //this replaces the a tags with Link, to move around pages. 
 import axios from 'axios';
 export default function Navbar(){
 
 
     //AXIOS used to GET data from API. NEEDS WORK!!!!! Link works but function itself not. 
-
-const getRecipes = () =>{
-   
-axios.get('https://api.spoonacular.com/recipes/random?apiKey=5a312119fa95425c8af9a9236717e2b5').then(function
-    (response){
-console.log(response);
+    
+const getRecipes = () => {
+   const apiKey = "5a312119fa95425c8af9a9236717e2b5"; 
+    axios.get('https://api.spoonacular.com/recipes/random?'+ apiKey).then(function
+        (response){
+        console.log(response);
     })
 }
 
     return <nav className="nav">
-        <Link to="/">Logo</Link>
+        <Link to="/"><img src={logo} alt="WhatsForDinner"/></Link>
         <ul>
             <li><Link to="/plan">Plan My Meals</Link></li>
             <li><Link to="/all">All Recipes</Link></li>
