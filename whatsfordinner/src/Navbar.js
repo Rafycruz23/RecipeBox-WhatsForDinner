2885f66f7802c67115c529bd3e8b74c8e3404409
Rafael <rafycruz23@live.com>
2023-03-05 12:34:00 -0500

    added Bootstrap searchbar to Navbar.js

--- whatsfordinner/src/Navbar.js
+++ whatsfordinner/src/Navbar.js
@@ -7,6 +7,10 @@ export default function Navbar(){
         <ul>
             <li><Link to="/plan">Plan My Meals</Link></li>
             <li><Link to="/all">All Recipes</Link></li>
+            <form class="d-flex" role="search">
+            <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </ul> 
     </nav>
 }
