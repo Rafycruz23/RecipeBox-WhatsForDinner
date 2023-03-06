98984c7420a955226fc4023baf429dd71e60eb61
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-06 11:31:53 -0500

    adds routing

--- whatsfordinner/src/App.js
+++ whatsfordinner/src/App.js
@@ -2,12 +2,20 @@
 import Navbar from "./Navbar";
 import CarouselSlide from "./CarouselSlide";
 import "./styles.css" 
+import {Route, Routes} from 'react-router-dom';
+import AllRecipes from './AllRecipes';
+import PlanMeals from './PlanMeals';
 
 
 export default function App() {
   return <div>
     <Navbar />
     <CarouselSlide />
+    <Route path='/PlanMeals' component={PlanMeals} />
   </div>
 };
 
+
+
+
+// export default App;
--- whatsfordinner/src/PlanMeals.js
+++ whatsfordinner/src/PlanMeals.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import "./styles.css" 
 
+import React from 'react';
 
 export default function PlanMeals(){
     return<div>
