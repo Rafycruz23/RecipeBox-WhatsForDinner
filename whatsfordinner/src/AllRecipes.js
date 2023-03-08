2cc3871eeb9c21ec86d6227796894b9c3f03a872
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-08 13:02:34 -0500

    adds comments to code

--- whatsfordinner/src/AllRecipes.js
+++ whatsfordinner/src/AllRecipes.js
@@ -5,13 +5,16 @@ import axios from "axios";
 
 export default function AllRecipes(){
 
-    const [randomRecipe] = useGetRecipes("");
+//when we call the useGetRecipes, we pass in whichever endpoint we want to call as the parameter, in this case the random endpoint.  
+
+    const [randomRecipe] = useGetRecipes("random");
     
     return (
       <div>
         <h1>AllRecipes is working</h1>
-        {/* <button onClick={recipe}>Click</button> */}
+        {/* <button onClick={useGetRecipes('random')}>Click</button> */}
         <p>{randomRecipe.title}</p>
+        <p>{randomRecipe.summary}</p>
       </div>
     );
 }
