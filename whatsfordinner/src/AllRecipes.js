7587f2a1b2a822732ce94a965fcf5851328bc231
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-08 11:34:57 -0500

    fixed state and api

--- whatsfordinner/src/AllRecipes.js
+++ whatsfordinner/src/AllRecipes.js
@@ -6,29 +6,25 @@ import axios from "axios";
 export default function AllRecipes(){
 
     // const [recipe] = useGetRecipes("");
-      const [recipe, setRecipe] = useState({});
+      const [newRecipe, setRecipe] = useState({});
 
     useEffect(() => {
       const fetchData = async () => {
         const response = await axios.get(
           "https://api.spoonacular.com/recipes/random?apiKey=5a312119fa95425c8af9a9236717e2b5"
         );
-        setRecipe(response.data);
-        console.log(response);
+        setRecipe(response.data.recipes[0]);
+        console.log(newRecipe.summary);
         
       };
       fetchData();
     }, []);
     
     return (
-    <div>
-  
-    <h1>AllRecipes is working</h1>
-    {/* <button onClick={recipe}>Click</button> */}
-    {/* <p>{recipe}</p>
-    <p>{recipe.summary}</p> */}
-
-        
-    </div> 
-    )
+      <div>
+        <h1>AllRecipes is working</h1>
+        {/* <button onClick={recipe}>Click</button> */}
+        <p>{newRecipe.title}</p>
+      </div>
+    );
 }
