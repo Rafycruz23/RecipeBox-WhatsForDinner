58a4ba709604a3de5a4fd91bd2e140b64a416360
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-14 13:59:45 -0400

    updates to useGetRecipes and planMeals

--- whatsfordinner/src/PlanMeals.js
+++ whatsfordinner/src/PlanMeals.js
@@ -15,3 +15,44 @@ export default function PlanMeals(){
       </div>
     );
     };
+import React, {useEffect, useState} from 'react';
+import useGetRecipes
+ from "./useGetRecipes";
+ import axios from "axios";
+
+export default function PlanMeals(){
+
+    // const [randomRecipe] = useGetRecipes("random");
+//     const [recipeSummary, setRecipeSummary] = useState("");
+
+//     useEffect(() => {
+//    const fetchData = async () => {
+//      const response = await axios.get(
+//        `https://api.spoonacular.com/recipes/${randomRecipe.id}/summary/?apiKey=8959366c723444e08598151392a0775e`
+//      );
+//      setRecipeSummary(response.data.recipes[0]);
+//    };
+//    fetchData();
+//    console.log(recipeSummary.summary);
+//  }, []);
+//  return [recipeSummary]
+
+    return(
+
+
+
+    <div>
+        {/* <h1>PlanMeals is working</h1> */}
+        <div className="card" style={{width: '18rem'}}>
+  <img src={randomRecipe.image} className="card-img-top" alt=""/>
+  <div className="card-body">
+    <h5 className="card-title">{randomRecipe.title}</h5>
+    <p className="card-text">{randomRecipe.summary}</p>
+    <a href={randomRecipe.sourceUrl} className="btn btn-primary" target="_blank">Get Recipe</a>
+  </div>
+</div>
+
+        
+    </div> 
+    )
+}
--- whatsfordinner/src/useGetRecipes.js
+++ whatsfordinner/src/useGetRecipes.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 //by passing in the parameter 'endpoint' we can specify from within each component the specific api endpoint we want to call. The parameter is then interpolated into the link
+//alternate API key: 8959366c723444e08598151392a0775e
+//apiKey=5a312119fa95425c8af9a9236717e2b5
 
  function useGetRecipes(endpoint) {
  const [newRecipe, setRecipe] = useState({});
@@ -11,6 +13,7 @@ import axios from "axios";
    const fetchData = async () => {
      const response = await axios.get(
        `https://api.spoonacular.com/recipes/${endpoint}?apiKey=`+ apiKey
+       `https://api.spoonacular.com/recipes/${endpoint}?apiKey=8959366c723444e08598151392a0775e`
      );
      setRecipe(response.data.recipes[0]);
    };
