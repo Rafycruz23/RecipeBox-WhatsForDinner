ee5a03697a1e17d204f6320569d1d9c90fd4e4e8
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-16 10:02:25 -0400

    updates plan meals to generate seven different recipe cards

--- whatsfordinner/src/PlanMeals.js
+++ whatsfordinner/src/PlanMeals.js
@@ -1,36 +1,22 @@
-// import "./styles.css" 
-// import React from 'react';
-// import useGetRecipes from "./useGetRecipes";
 
-// export default function PlanMeals(){
-//     const [randomRecipe] = useGetRecipes("random");
-//     console.log(randomRecipe)
-    
-//     return (
-//       <div>
-//         <h1>PlanMeals is working</h1>
-//         {/* <button onClick={useGetRecipes('random')}>Click</button> */}
-//         <p>{randomRecipe.title}</p>
-//         <p>{randomRecipe.summary}</p>
-//       </div>
-//     );
-//     };
 import React from 'react';
 import useGetRecipes from "./useGetRecipes";
  
 
 export default function PlanMeals(){
 
-    const [randomRecipe] = useGetRecipes("random");
+  //by passing in the parameter of 7, it is used in the API as the count to generate 7 recipes
+    const [randomRecipe] = useGetRecipes("random", 7);
+    
    
     return(
 
-//this is taking the randomRecipe object and converting it to an array so that it can be mapped over. However, it is only generating one recipe and displaying it 7 times
+      //this works, but looks terrible!
         <div>
-        {Object.values(randomRecipe).slice(0, 7).map(recipe => (
+        {randomRecipe.map(recipe => (
    
             <div className="card" style={{width: '18rem'}}>     
-            <img src={randomRecipe.image} className="card-img-top" alt=""/>
+            <img src={recipe.image} className="card-img-top" alt=""/>
             <div className="card-body">
               <h5 className="card-title">{recipe.title}</h5>
               <p className="card-text">{recipe.summary}</p>
