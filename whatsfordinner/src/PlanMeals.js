b2fc2948f02772325b28ee081c2058eeea662387
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-03-21 15:37:22 -0400

    removes HTML tags from summary

--- whatsfordinner/src/PlanMeals.js
+++ whatsfordinner/src/PlanMeals.js
@@ -9,26 +9,35 @@ export default function PlanMeals(){
   //by passing in the parameter of 7, it is used in the API as the count to generate 7 recipes
     const [randomRecipe] = useGetRecipes("random", 7);
     
+    //this function removes the HTML tags from the summary portion of the recipe. It is called in the text portion of the card
+    function removeTags(text) {
+      return text.replace(/<\/?[^>]+(>|$)/g, "");
+    }
+  
    
-    return(
-
+    return (
       //this works, but looks terrible!
-        <div className='plan-meal'>
-        {randomRecipe.map(recipe => (
-   
-            <Card className="card-container">     
-            <Card.Img src={recipe.image} className="card-image" alt=""/>
+      <div className="plan-meal">
+        
+        {randomRecipe.map((recipe) => (
+          <Card className="card-container">
+            <Card.Img src={recipe.image} className="card-image" alt="" />
             <Card.Body>
               <Card.Title className="card-title">{recipe.title}</Card.Title>
-              <Card.Text className="card-text">{recipe.summary}</Card.Text>
-              <Card.Link href={recipe.sourceUrl} className="btn btn-primary" target="_blank">Get Recipe</Card.Link>
+              <Card.Text className="card-text">{removeTags(recipe.summary)}</Card.Text>
+              <Card.Link
+                href={recipe.sourceUrl}
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Get Recipe
+              </Card.Link>
             </Card.Body>
-            </Card>
-            ))};
-  </div>
-
-    
-  );
+          </Card>
+        ))}
+        ;
+      </div>
+    );
 }
 
     
