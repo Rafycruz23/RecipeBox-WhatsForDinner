affdde5dbe1302789f582432da66870b604d0d21
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-04-02 12:39:28 -0400

    adds button to regenerate recipes, but not working

--- whatsfordinner/src/PlanMeals.js
+++ whatsfordinner/src/PlanMeals.js
@@ -23,37 +23,51 @@ export default function PlanMeals(){
       setDisplay (!display);
      }
    
+     //button doesn't work to re-generate the new recipes. 
     return (
-      
       <div className="plan-meal">
         <div>
-        <h3>Here are four recipes to give you some culinary inspiration. Need more ideas?</h3>
-        
-        <button className="btn btn-light"
-            style={{ color: "#73a580", border: "solid #73a580" }} onClick={randomRecipe}>More Recipes</button>
+          <h3>Here are four recipes to give you some culinary inspiration.</h3>
+          <span>Need more ideas?</span>
+
+          <button
+            className="btn btn-light"
+            style={{ color: "#73a580", border: "solid #73a580" }}
+            onClick={useGetRecipes("random", 4)}
+          >
+            More Recipes
+          </button>
         </div>
-        
         {randomRecipe.map((recipe) => (
           <Card className="card-container">
             <Card.Img src={recipe.image} className="card-image" alt="" />
             <Card.Body>
               <Card.Title className="card-title">{recipe.title}</Card.Title>
               <Card.Text className="card-text">
-              {removeTags(recipe.summary)}
+                {removeTags(recipe.summary)}
               </Card.Text>
-              <div> 
-              <br></br>
-             {!display && (
-              <Card.Text><ul>Ingredients: {recipe.extendedIngredients.map((ingredients)=><li>{ingredients.name}</li>)}</ul></Card.Text>
-             )}
-              <br></br>
-            </div>
-            <div> 
-             {!display && (
-              <Card.Text>{removeTags(recipe.instructions)}</Card.Text>
-             )}
-            </div>
-            <Button variant="primary" onClick={handleClick}>Get Recipe</Button>
+              <div>
+                <br></br>
+                {!display && (
+                  <Card.Text>
+                    <ul>
+                      Ingredients:{" "}
+                      {recipe.extendedIngredients.map((ingredients) => (
+                        <li>{ingredients.name}</li>
+                      ))}
+                    </ul>
+                  </Card.Text>
+                )}
+                <br></br>
+              </div>
+              <div>
+                {!display && (
+                  <Card.Text>{removeTags(recipe.instructions)}</Card.Text>
+                )}
+              </div>
+              <Button variant="primary" onClick={handleClick}>
+                Get Recipe
+              </Button>
             </Card.Body>
           </Card>
         ))}
